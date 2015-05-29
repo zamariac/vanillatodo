4d29d9e9126d533745e7fb6f61e5206018613eb5
Maria Zamora <Zamora.Mariac@gmail.com>
2015-05-28 19:10:00 -0500

    working on it

---
+++ dist/vanillatodo.js
@@ -0,0 +1,31 @@
+var myButton  = "button"
+var todoInput = "input"
+var	todoForm = "form"
+
+var todoList = []
+
+	function onButtonClick(e){
+
+
+
+myButton.addEventListener('todoInput', onButtonClick);
+
+var myButton = document.getElementById('input')
+
+
+}
+
+
+//     function onButtonClick(eventObj) {
+    
+//   document.getElementById('To do list').style.fontStyle="italic";
+// }
+
+// var myButton = document.getElementById('To do list');
+
+// myButton.addEventListener('To do list', onButtonClick);
+
+// var parent = document.getElementById('parent');
+// parent.addEventListener('click', onButtonClick);    
+
+    
